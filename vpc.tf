55d274296162cafc1a8670a6468e2db8bcda56cd
abdisuhanova <93474979+abdisuhanova@users.noreply.github.com>
2023-03-31 14:47:19 +0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,15 +10,7 @@ variable "region" {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-# VPC
-resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
-}
+  credentials = file(${MY_FILE})
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
