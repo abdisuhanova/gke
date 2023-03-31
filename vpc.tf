648e9ff94e13583a0acb9f49a7488ab1747733ec
abdisuhanova <93474979+abdisuhanova@users.noreply.github.com>
2023-03-31 16:56:38 +0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,44 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
-variable "project_id" {
-  description = "project id"
+variable "cred" {
+type = string
+}
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
 }
 
-variable "region" {
-  description = "region"
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
 }
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+      preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+provider "google" {
+  project = "playground-s-11-e0cdb590"
+  region  = "us-central1"
+  credentials = file(var.cred)
+}
+
