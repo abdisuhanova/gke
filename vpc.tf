5f15eec9f3b7c761ba575a468d3d9b37f28a852b
abdisuhanova <93474979+abdisuhanova@users.noreply.github.com>
2023-03-31 15:50:49 +0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,16 +8,3 @@ variable "project_id" {
 variable "region" {
   description = "region"
 }
-resource "google_compute_network" "vpc" {
-  project                 = "playground-s-11-e0cdb590"
-  name                    = "vpc-network"
-  auto_create_subnetworks = true
-  mtu                     = 1460
-}
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
-}
